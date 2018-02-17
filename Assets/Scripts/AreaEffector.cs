353862eaa6e5d0e94bf7564d563b72213046fb2c
marlord <marlon_dx@hotmail.com>
2018-02-17 13:13:22 -0600

    Inciso 2.3 terminado

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+    float time=0; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time+= Time.deltaTime;
+        if (time >= 20f)
+        {
+            gameObject.SetActive(false);
+            
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+        Debug.Log(time);
+        
+	}
+}
